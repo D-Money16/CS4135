c7124fb09f0fb77a8daaec77537c39d6764766dd
MLmarbles <22338705@studentmail.ul.ie>
2026-02-19 15:48:10 +0000

    Spring setup

---
+++ backend/cs4135spring/src/test/java/ie/ul/cs4135spring/Cs4135springApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.ul.cs4135spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs4135springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
