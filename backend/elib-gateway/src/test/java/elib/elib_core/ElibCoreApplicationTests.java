2de59ca15c8cec008984916fc7487a9f06d7ed2e
Daragh <22351159@studentmail.ul.ie>
2026-04-05 18:28:11 +0100

    init

---
+++ backend/elib-gateway/src/test/java/elib/elib_core/ElibCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package elib.elib_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
