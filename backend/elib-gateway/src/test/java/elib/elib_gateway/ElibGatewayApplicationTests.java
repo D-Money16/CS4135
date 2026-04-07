e81034e9be42416b8045fdd332e831f6315ae7f2
MLmarbles <22338705@studentmail.ul.ie>
2026-04-07 21:13:33 +0100

    Docker file and connection to PostgreSQL

--- backend/elib-gateway/src/test/java/elib/elib_gateway/ElibGatewayApplicationTests.java
+++ backend/elib-gateway/src/test/java/elib/elib_gateway/ElibGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package elib.elib_core;
+package elib.elib_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ElibCoreApplicationTests {
+class ElibGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
--- backend/elib-support/src/test/java/elib/elib_support/ElibSupportApplicationTests.java
+++ backend/elib-support/src/test/java/elib/elib_support/ElibSupportApplicationTests.java
@@ -1,4 +1,4 @@
-package elib.elib_core;
+package elib.elib_support;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +9,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
-class ElibCoreApplicationTests {
+class ElibSupportApplicationTests {
 
 	@Container
 	@ServiceConnection
