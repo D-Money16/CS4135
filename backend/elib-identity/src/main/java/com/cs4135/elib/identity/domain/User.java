0d88333fefa93820f3f0747de8a8b3d39fbe71a5
JoeConsidine <150838463+JoeConsidine@users.noreply.github.com>
2026-04-15 23:00:40 +0100

    Changed User in Identity

--- backend/elib-identity/src/main/java/com/cs4135/elib/identity/domain/User.java
+++ backend/elib-identity/src/main/java/com/cs4135/elib/identity/domain/User.java
@@ -1,5 +1,6 @@
 package com.cs4135.elib.identity.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,8 @@ public class User {
 
     private String username;
     private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Enumerated(EnumType.STRING)
